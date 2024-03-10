651b05c610e1ff4926e1a3784a85cfa4835570dd
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-03-10 21:28:21 +0000

    chore wrote tests for city model

--- tests/test_city_model.py
+++ tests/test_city_model.py
@@ -14,3 +14,24 @@ class TestCity(unittest.TestCase):
         self.city = City()
         self.city.name = ""
         self.city.state_id = ""
+
+    def test_attributes(self):
+        """Test that the appropriate attributes are set"""
+
+        self.assertTrue(hasattr(self.city, "id"))
+        self.assertTrue(hasattr(self.city, "created_at"))
+        self.assertTrue(hasattr(self.city, "updated_at"))
+        self.assertTrue(hasattr(self.city, "state_id"))
+        self.assertEqual(self.city.state_id, "")
+        self.assertTrue(hasattr(self.city, "name"))
+        self.assertEqual(self.city.name, "")
+
+    def test_create_city(self):
+        """Test creating a new city"""
+        self.assertEqual(str(type(self.city)), "<class 'models.city.City'>")
+        self.assertIsInstance(self.city, City)
+        self.assertTrue(issubclass(type(self.city), BaseModel))
+
+    def test_new_instance_stored_in_objects(self):
+        """Test new instance is stored in storage"""
+        self.assertIn(self.city, storage.all().values())
